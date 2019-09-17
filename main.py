c903e017ca560f5cdc9778eee95c1dead58c78ef
BesedaDi <dianabesedina223@gmail.com>
2019-09-17 21:36:45 +0700

    project start.

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
-# Case 1
+# Case-study #1
+# Developers:   Besedina D. (%),
+#               Zaitseva A. (%),
+#               Kuzmin E. (%)
+import turtle
 
-# Task 1
+
+def drawin1 ():
+# TODO: (D) Paint a написать какие конкретно рисунки.
+    pass
+def drawin2 ():
+# TODO: (D) Paint a написать какие конкретно рисунки.
+    pass
+def drawin3 ():
+# TODO: (D) Paint a написать какие конкретно рисунки.
+    pass
+def drawin4():
+#TODO: (A) Paint a какие рисунки.
+    pass
+def drawin5():
+#TODO: (A) Paint a какие рисунки.
+    pass
+def drawin6():
+#TODO: (E) Paint a какие рисунки.
+    pass
+def drawin7 ():
+#TODO: (E) Paint a какие рисунки.
+    pass
+def square (x,y,a):
+    '''
+    Function,
+    :param x:
+    :param y:
+    :param a:
+    :return:
+    '''
+def main():
+    drawin1()
+    drawin2()
+    drawin3()
+    drawin4()
+    drawin5()
+    drawin6()
+    drawin7()
+
+    pass
