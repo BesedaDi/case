4b643983317a2e2acd570c6ec3e1e6e9dc9ed1e0
BesedaDi <dianabesedina223@gmail.com>
2019-09-21 17:45:02 +0700

    project start.

--- копия main.py
+++ копия main.py
@@ -10,7 +10,26 @@ import turtle
  def triangle(x, y, a, b, color):
      # TODO: (D) Paint a triangle.
 
-  
+     '''
+    Function, drawing triangle.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a triangle
+    :param b: side length of the diagonal of the triangle
+    :return: None
+    '''
+
+     turtle.up()
+     turtle.setposition(x, y)
+     turtle.down()
+     turtle.color(color)
+     turtle.begin_fill()
+     turtle.forward(a)
+     turtle.right(90)
+     turtle.forward(a)
+     turtle.right(135)
+     turtle.forward(b)
+     turtle.end_fill()
  pass
 
 def parallelogram(x, y, a1, b1, color):
