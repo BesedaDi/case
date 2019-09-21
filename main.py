4d155cf7037ae41a9f1ca63cb6de704913e05a94
BesedaDi <dianabesedina223@gmail.com>
2019-09-21 17:52:40 +0700

    project start.

--- main.py
+++ main.py
@@ -9,38 +9,52 @@ import turtle
 
  def triangle(x, y, a, b, color):
      # TODO: (D) Paint a triangle.
-
-  
- pass
+pass
 
 def parallelogram(x, y, a1, b1, color):
-# TODO: (E) Paint a parallelogram.
-    pass
+     # TODO: (E) Paint a parallelogram.
+pass
 
 def square(x,y, a3):
-#TODO: (A) Paint  a square.
-    pass
+     # TODO: (A) Paint  a square.
+pass
+
 def figure1():
-# TODO: (D) Paint a figure1.
+     # TODO: (D) Paint a figure1.
 pass
+
 def figure2():
-# TODO: (D) Paint a figure2.
+     # TODO: (D) Paint a figure2.
 pass
+
 def figure3():
-# TODO: (D) Paint a figure3.
+     # TODO: (D) Paint a figure3.
 pass
+
 def figure4():
-# TODO: (E) Paint a figure4.
+     # TODO: (E) Paint a figure4.
 pass
+
 def figure5():
-# TODO: (E) Paint a figure5.
+     # TODO: (E) Paint a figure5.
 pass
+
 def figure6():
-# TODO: (A) Paint a figure6.
+     # TODO: (E) Paint a figure6.
 pass
+
 def figure7():
-# TODO: (A) Paint a figure7.
+     # TODO: (A) Paint a figure7.
 pass
+
+def figure8():
+     # TODO: (A) Paint a figure8.
+pass
+
+def figure9():
+     # TODO: (A) Paint a figure9.
+pass
+
 def main():
     triangle(-200, 200, 180, 260, 'red')
     turtle.done()
